21a999396497aebb038aad98a8fa8ebe37878a12
Alejo <alejito23001@gmail.com>
2023-02-17 23:46:18 -0500

    NEW: New options to manage files

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,12 +1,13 @@
-vim.g.mapleader="<space>"
-
 local opt = {
     timeoutlen=239,
     number=true,
     tabstop=4,
     shiftwidth=4,
     expandtab=true,
-    termguicolors=true
+    termguicolors=true,
+    ignorecase=true,
+    smartcase=true,
+    undofile = true,
 }
 
 vim.cmd[[ colorscheme onedark ]]
@@ -14,3 +15,12 @@ vim.cmd[[ colorscheme onedark ]]
 for key, value in pairs(opt) do
     vim.o[key] = value
 end
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
