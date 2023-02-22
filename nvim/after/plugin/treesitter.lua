3a110a3b15b6a6c76dc3c5995d391e24c6b05a44
Alejo <alejito23001@gmail.com>
2023-02-21 19:22:46 -0500

    NEW: Added languanges table as common variable

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "python", "java", "javascript", "typescript", "lua", "vim", "help" },
+  ensure_installed = require("common").languages,
   sync_install = true,
   auto_install = true,
   ignore_install = {  },
---
+++ nvim/lua/common.lua
@@ -0,0 +1,5 @@
+local M = {}
+
+M.languages = { "python", "java", "javascript", "typescript", "lua", "vim", "help" }
+
+return M
